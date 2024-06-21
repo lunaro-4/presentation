a9acea72183cac76eec5277358c5ceb3b04b6a3e
lunaro-4 <pavelike@gmail.com>
2024-06-21 09:20:56 +0300

    feat: add main file; tweak css

--- index.js
+++ index.js
@@ -4,9 +4,40 @@ import fs from 'fs'
 // 1. Create instance (with options if you want)
 const marpit = new Marpit.Marpit()
 
+//
+	// :root{
+	// 	--prim: #F8F6E3;
+	// 	/*--sec: #97E7E1;
+	// 	--tint: #6AD4DD;
+	// 	--acc: #7AA2E3; */
+	// 	--sec: #83B4FF;
+	// 	--tint: #5A72A0;
+	// 	--acc: #1A2130; 
+	//
+	// }
 // 2. Add theme CSS
 const theme = `
-/* @theme empty */
+/* @theme my-theme */
+	:root{
+		--back: #FED8B1;
+		--head: #6F4E37;
+		--sec: #A67B5B;
+		--text: #1A2130; 
+		--acc: #ECB176;
+
+	}
+
+	section {
+		font-size: 30px;
+		font: 1rem "Fira Sans", sans-serif;;
+		padding: 40px;
+		background-color: var(--back);
+		color: var(--text);
+		color: #000;
+	}
+	h1 {
+		color: var(--head);
+	}
 `
 // `
 // /* @theme example */
